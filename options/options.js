6f988309cdf7aef68a19878788a38ad06dde5baf
Crowdbotics <team@crowdbotics.com>
2024-12-11 13:36:33 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "etmanacommunity-50281",
+  url: "https://etmanacommunity-50281.botics.co",
+  api: "https://etmanacommunity-50281.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
